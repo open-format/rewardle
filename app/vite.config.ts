35b1c82c28e8c9dba4c325aacc7f809023ed147c
Andy <andy@openformat.tech>
2023-12-13 15:33:06 +0000

    Update vite config and tsconfig

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [tsconfigPaths()],
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      port: 3000,
+    },
+    plugins: [tsconfigPaths()],
+    define: {
+      "process.env.APP_ID": JSON.stringify(env.APP_ID),
+    },
+  };
 });
