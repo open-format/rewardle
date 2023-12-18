35be2b30832a639271155f1ab5f04c0cb7551827
Andy <andy@openformat.tech>
2023-12-18 12:53:04 +0000

    adds cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import rewards from "./routes/api/v1/rewards";
 
 const app = new Hono().basePath("/api/v1");
+
+app.use("*", cors({ origin: ["http://localhost:3000"] }));
 
 app.get("/ping", (c) => c.text("👋"));
 app.route("/rewards", rewards);
