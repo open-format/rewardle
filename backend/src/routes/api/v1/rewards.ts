81bb5352eed13c400073d5f83810d06eea001a78
Andy <andy@openformat.tech>
2023-12-18 12:10:08 +0000

    Simplify API endpoints

--- backend/src/routes/api/v1/rewards.ts
+++ backend/src/routes/api/v1/rewards.ts
@@ -1,12 +1,6 @@
-import {
-  ActivityType,
-  Chains,
-  OpenFormatSDK,
-  RewardTriggerParams,
-  RewardType,
-  toWei,
-} from "@openformat/sdk";
+import { Chains, OpenFormatSDK } from "@openformat/sdk";
 import { Hono } from "hono";
+import TokenSystem from "../../../utils/tokenSystem";
 
 enum Status {
   SUCCESS = "success",
@@ -26,116 +20,41 @@ rewards.onError((err, c) => {
   return c.json({ status: "Failed", message: err.message }, 500);
 });
 
-rewards.post("/actions/xp/award", async (c) => c.text(""));
-rewards.post("/actions/reward-token/award", async (c) => c.text(""));
-rewards.post("/actions/badges/:id/award", async (c) => c.text(""));
-rewards.post("/missions/xp/award", async (c) => c.text(""));
-rewards.post("/missions/reward-token/award", async (c) => c.text(""));
-rewards.post("/missions/badges/:id/award", async (c) => c.text(""));
-rewards.post("/batch-award", async (c) => c.text(""));
-
 /**
- * POST rewards/xp/award
+ * Triggers the token system reward process based on a user's completed action.
  *
- * @summary Awards XP tokens to a user based on an action.
- * @tags rewards
- * @param {string} request.body.receiver - A Valid ethereum address of the receiver of the XP Tokens
- * @param {string} request.body.action_id - ID of the action triggering the reward
- * @return {object} 200 - Success response - application/json
+ * This endpoint processes a user's action and calculates the appropriate reward
+ * using the token system. It checks for completed missions and triggers rewards accordingly.
+ * See `action.json` and `mission.json` for action and mission configuration.
+ *
+ * @route POST rewards/token-system/trigger
+ *
+ * @param {string} c.req.json().user_address - The blockchain address of the user.
+ * @param {string} c.req.json().action_id - The unique identifier of the completed action.
+ * @returns {Promise<Object>} The JSON response containing the status, transaction URL, and rewards.
+ *
+ * @example
+ * curl --location 'http://localhost:8080/api/v1/rewards/token-system/trigger' \
+ * --header 'Content-Type: application/json' \
+ * --data '{
+ *    "user_address": "0x03755352654D73DA06756077Dd7f040ADcE3Fd58",
+ *    "action_id": "test_action"
+ * }'
  */
+rewards.post("/token-system/trigger", async (c) => {
+  const { user_address, action_id } = await c.req.json();
 
-rewards.post("/xp/award", async (c) => {
-  const { receiver, action_id } = await c.req.json();
+  const tokenSystem = new TokenSystem(sdk);
 
-  const params: RewardTriggerParams = {
-    receiver,
-    tokens: [
-      {
-        id: action_id,
-        address: process.env.XP_TOKEN_ID as string,
-        amount: toWei("10"),
-        activityType: ActivityType.ACTION,
-        type: RewardType.XP_TOKEN,
-      },
-    ],
-  };
-
-  const reward = await sdk.Reward.trigger(params);
+  const reward = await tokenSystem.handleCompletedAction(
+    user_address,
+    action_id
+  );
 
   return c.json({
     status: Status.SUCCESS,
     transaction: `https://mumbai.polygonscan.com/tx/${reward.transactionHash}`,
-  });
-});
-
-/**
- * POST /rewards/reward-token/award
- *
- * @summary Awards a reward token to a user for completing a mission.
- * @tags rewards
- * @param {object} request.body.required - The receiver and action ID
- * @param {string} request.body.receiver - A Valid ethereum address of the receiver of the Reward Tokens
- * @param {string} request.body.action_id - ID of the action triggering the reward
- * @return {object} 200 - Success response - application/json
- */
-
-rewards.post("/reward-token/award", async (c) => {
-  const { receiver, action_id } = await c.req.json();
-
-  const params: RewardTriggerParams = {
-    receiver,
-    tokens: [
-      {
-        id: action_id,
-        address: process.env.REWARD_TOKEN_ID as string,
-        amount: 1,
-        activityType: ActivityType.MISSION,
-        type: RewardType.CONSTELLATION_TOKEN,
-      },
-    ],
-  };
-
-  const reward = await sdk.Reward.trigger(params);
-
-  return c.json({
-    status: Status.SUCCESS,
-    transaction: `https://mumbai.polygonscan.com/tx/${reward.transactionHash}`,
-  });
-});
-
-/**
- * POST /badges/:id/award
- *
- * @summary Awards a specific badge to a user for completing a mission.
- * @tags rewards
- * @param {string} id.path.required - ID of the badge
- * @param {string} request.body.receiver - A Valid ethereum address of the receiver of the Badge
- * @param {string} request.body.action_id - ID of the action triggering the award
- * @return {object} 200 - Success response - application/json
- */
-
-rewards.post("/badges/:id/award", async (c) => {
-  const badgeAddress = c.req.param("id");
-  const { receiver, action_id } = await c.req.json();
-
-  const params: RewardTriggerParams = {
-    receiver,
-    tokens: [
-      {
-        id: action_id,
-        address: badgeAddress,
-        amount: 1,
-        activityType: ActivityType.MISSION,
-        type: RewardType.BADGE,
-      },
-    ],
-  };
-
-  const reward = await sdk.Reward.trigger(params);
-
-  return c.json({
-    status: Status.SUCCESS,
-    transaction: `https://mumbai.polygonscan.com/tx/${reward.transactionHash}`,
+    rewards: reward.rewarded,
   });
 });
 
