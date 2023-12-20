e4f2d25f1183c1ba7a19e11cf76dc340623b6a96
Eugene Getov <eugene.getov@openformat.tech>
2023-12-20 10:04:26 +0000

    - Added @openformat/react@1.3.0-dev-12e705d - Added LoginButton component with our useWallet ConnectButton and useSetIsWalletModalOpen, as in the dashboard app

---
+++ app-new/components/LoginButton.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  ConnectButton,
+  useSetIsWalletModalOpen,
+  useWallet,
+} from "@openformat/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function LoginButton() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const validSession = session?.user?.address;
+  const { address } = useWallet();
+  const prevAddress = session?.user?.address;
+  const setIsWalletModalOpen = useSetIsWalletModalOpen();
+
+  useEffect(() => {
+    const lastConnectedWallet = JSON.parse(
+      localStorage.getItem(
+        "__TW__/coordinatorStorage/lastConnectedWallet"
+      ) as any
+    );
+    const localWallet = JSON.parse(
+      localStorage.getItem("__TW__/localWallet/localWalletData") as any
+    );
+
+    if (
+      validSession &&
+      router.pathname !== "/login" &&
+      lastConnectedWallet?.walletId !== "metamask" &&
+      localWallet
+    ) {
+      setIsWalletModalOpen(true);
+    }
+  }, [validSession]);
+
+  useEffect(() => {
+    if (prevAddress && address !== prevAddress) {
+      router.replace("/logout");
+    }
+  }, [address]);
+
+  return (
+    <ConnectButton
+      switchToActiveChain={true}
+      modalSize="compact"
+      welcomeScreen={{
+        title: "Connect your wallet",
+        subtitle: "Connect with Metamask or Continue as Guest.",
+      }}
+      modalTitleIconUrl={"https://app.openformat.tech/favicon.ico"}
+    />
+  );
+}
--- app-new/pages/index.tsx
+++ app-new/pages/index.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Leaderboard from "../components/Leaderboard";
 import LogoutButton from "../components/LogoutButton";
+import LoginButton from "../components/LoginButton";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to OPENFORMAT GetStarted!</h1>
       <Leaderboard />
+      <LoginButton></LoginButton>
       <LogoutButton></LogoutButton>
     </div>
   );
