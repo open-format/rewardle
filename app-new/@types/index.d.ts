4d4622198ddc4b876c1e403e36dc8e622d3796ec
Eugene Getov <eugene.getov@openformat.tech>
2023-12-21 09:46:31 +0000

    - Removed awardedBadges and completedActions from set and refresh profile data. - Updated the ProfileData type to just contain nickname, eth_address and email_address

--- app-new/@types/index.d.ts
+++ app-new/@types/index.d.ts
@@ -16,16 +16,6 @@ interface LeaderboardEntry {
 }
 
 export interface ProfileData {
-  actions: ActivityAction[];
-  missions: ActivityMission[];
-  accessKeys: AccessKey[];
-  xpAwarded: number;
-  rewardTokensAwarded: number;
-  hasAccessKey: boolean;
-  receivedAccessKeys: boolean;
-  totalCompletedMissions: number;
-  discord_user_id: string;
-  discord_user_name: string;
   nickname: string;
   eth_address: string;
   email_address: string;
--- app-new/stores/useProfileStore.tsx
+++ app-new/stores/useProfileStore.tsx
@@ -26,15 +26,11 @@ export const useProfileStore = create<ProfileState>(
         const res = await apiClient.get("/profile/me");
         set({
           profileData: res.data,
-          awardedBadges: getAwardedBadges(res.data),
-          completedActions: getCompletedActions(res.data),
         });
       },
       setProfileData: (profileData) =>
         set({
           profileData,
-          awardedBadges: getAwardedBadges(profileData),
-          completedActions: getCompletedActions(profileData),
         }),
     }),
     {
@@ -46,43 +42,3 @@ export const useProfileStore = create<ProfileState>(
     }
   )
 );
-
-function getAwardedBadges(profileData: ProfileData) {
-  const newAwardedBadges = new Set();
-
-  profileData?.missions?.forEach((res) => {
-    const foundBadge = MISSIONS.find(
-      (badge) => badge.id === res.metadata.name
-    );
-    if (foundBadge) {
-      newAwardedBadges.add(foundBadge.id);
-    }
-  });
-
-  return newAwardedBadges;
-}
-
-function getCompletedActions(profileData: ProfileData): {
-  [missionId: string]: string[];
-} {
-  const newCompletedActionsByMission: {
-    [missionId: string]: string[];
-  } = {};
-
-  MISSIONS.forEach((mission) => {
-    const completedActions = mission.requirements
-      ?.filter((req) => {
-        const actionsForReq = profileData?.actions?.filter(
-          (action) => action.metadata.name === req.actionId
-        );
-        return actionsForReq?.length >= req.count;
-      })
-      .map((req) => req.actionId);
-
-    if (completedActions.length > 0) {
-      newCompletedActionsByMission[mission.id] = completedActions;
-    }
-  });
-
-  return newCompletedActionsByMission;
-}
