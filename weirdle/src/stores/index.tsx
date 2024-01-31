99848f4d17c88125510f9b4b9cf450e6f0b7079b
Andy <andy@openformat.tech>
2024-01-31 17:02:21 +0000

    Add updateProfileData to ProfileStore

--- weirdle/src/stores/index.tsx
+++ weirdle/src/stores/index.tsx
@@ -1,9 +1,11 @@
-import { ProfileData } from "../@types";
+import apiClient from "utils/apiClient";
 import create from "zustand";
+import { ProfileData } from "../@types";
 
 interface ProfileState {
   profileData: ProfileData | null;
   setProfileData: (data: ProfileData) => void;
+  updateProfileData: () => void;
   resetProfileData: () => void;
 }
 
@@ -11,4 +13,9 @@ export const useProfileStore = create<ProfileState>((set) => ({
   profileData: null,
   setProfileData: (data) => set({ profileData: data }),
   resetProfileData: () => set({ profileData: null }),
+  updateProfileData: async () => {
+    await apiClient.get("/profile/me").then((res) => {
+      set({ profileData: res.data.data });
+    });
+  },
 }));
