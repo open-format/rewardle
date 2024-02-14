e462a372ccf5c40b9ba3f7b48989cc35ba4cf1cb
Andy <andy@openformat.tech>
2024-02-14 12:29:55 +0000

    add new actions and missions

--- backend/src/constants/missions.ts
+++ backend/src/constants/missions.ts
@@ -1,89 +1,206 @@
 export default [
   {
-    id: "Guess in One",
-    description: "Guess the word with one go",
+    id: "New Kid on the Block",
+    description: "Login to Rewardle for the first time",
     tokens: [
       {
         address: process.env.XP_TOKEN_ID,
-        amount: 100,
+        amount: 50,
       },
       {
         address: process.env.REWARD_TOKEN_ID,
-        amount: 5,
+        amount: 50,
       },
     ],
-    badgeUrl: "https://i.postimg.cc/3wcsFwRt/guess-in-one-badge.png",
+    badgeUrl:
+      "https://duqswponteoedwsylzwn.supabase.co/storage/v1/object/public/badges/New%20Kid%20On%20the%20Block.png",
     requirements: [
       {
-        actionId: "one_guess",
-        description: "Guess in a single go!",
+        actionId: "create_profile",
+        description: "Login to Rewardle for the first time",
         count: 1,
       },
     ],
   },
   {
-    id: "Naughty Naughty",
-    description: "Log three naughty words",
+    id: "Identity Established",
+    description: "Add your name to your profile",
     tokens: [
       {
         address: process.env.XP_TOKEN_ID,
-        amount: 100,
+        amount: 50,
       },
       {
         address: process.env.REWARD_TOKEN_ID,
-        amount: 5,
+        amount: 150,
       },
     ],
-    badgeUrl: "https://i.postimg.cc/BQ5GfsYv/naughty-word-badge.png",
+    badgeUrl: "",
     requirements: [
       {
-        actionId: "naughty_word",
-        description: "Complete the naughty_word twice!",
-        count: 2,
-      },
-    ],
-  },
-  {
-    id: "20 Day Streak",
-    description: "Play 20 days in a row",
-    tokens: [
-      {
-        address: process.env.XP_TOKEN_ID,
-        amount: 100,
-      },
-      {
-        address: process.env.REWARD_TOKEN_ID,
-        amount: 5,
-      },
-    ],
-    badgeUrl: "https://i.postimg.cc/cJCq33sK/20-day-streak-badge.png",
-    requirements: [
-      {
-        actionId: "streak_20",
-        description: "Add to the streak",
+        actionId: "add_name",
+        description: "Add your name to your profile",
         count: 1,
       },
     ],
   },
   {
-    id: "10 Day Streak",
-    description: "Play 10 days in a row",
+    id: "10 Wins",
+    description: "Win the game a total of 10 times",
     tokens: [
       {
         address: process.env.XP_TOKEN_ID,
-        amount: 100,
+        amount: 1500,
       },
       {
         address: process.env.REWARD_TOKEN_ID,
-        amount: 5,
+        amount: 200,
       },
     ],
-    badgeUrl: "https://i.postimg.cc/0jD1Qq9c/10-day-streak-badge.png",
+    badgeUrl:
+      "https://duqswponteoedwsylzwn.supabase.co/storage/v1/object/public/badges/10%20wins.png",
     requirements: [
       {
-        actionId: "streak_10",
-        description: "Add to the streak",
+        actionId: "win",
+        description: "Win the game",
+        count: 10,
+      },
+    ],
+  },
+  {
+    id: "25 Wins",
+    description: "Win the game a total of 25 times",
+    tokens: [
+      {
+        address: process.env.XP_TOKEN_ID,
+        amount: 4000,
+      },
+      {
+        address: process.env.REWARD_TOKEN_ID,
+        amount: 500,
+      },
+    ],
+    badgeUrl:
+      "https://duqswponteoedwsylzwn.supabase.co/storage/v1/object/public/badges/25%20wins.png",
+    requirements: [
+      {
+        actionId: "win",
+        description: "Win the game",
+        count: 25,
+      },
+    ],
+  },
+  {
+    id: "Half-Century Hero",
+    description: "Win the game a total of 50 times",
+    tokens: [
+      {
+        address: process.env.XP_TOKEN_ID,
+        amount: 10000,
+      },
+      {
+        address: process.env.REWARD_TOKEN_ID,
+        amount: 1000,
+      },
+    ],
+    badgeUrl:
+      "https://duqswponteoedwsylzwn.supabase.co/storage/v1/object/public/badges/50%20wins.png",
+    requirements: [
+      {
+        actionId: "win",
+        description: "Win the game",
+        count: 50,
+      },
+    ],
+  },
+  {
+    id: "First Time Lucky...",
+    description: "Win the game on your first guess once",
+    tokens: [
+      {
+        address: process.env.XP_TOKEN_ID,
+        amount: 5000,
+      },
+      {
+        address: process.env.REWARD_TOKEN_ID,
+        amount: 2000,
+      },
+    ],
+    badgeUrl:
+      "https://duqswponteoedwsylzwn.supabase.co/storage/v1/object/public/badges/First%20time%20lucky.png",
+    requirements: [
+      {
+        actionId: "first_guess",
+        description: "Win the game on your first guess",
         count: 1,
+      },
+    ],
+  },
+  {
+    id: "No Freaking Way",
+    description: "Win the game on your first guess 10 times",
+    tokens: [
+      {
+        address: process.env.XP_TOKEN_ID,
+        amount: 100000,
+      },
+      {
+        address: process.env.REWARD_TOKEN_ID,
+        amount: 20000,
+      },
+    ],
+    badgeUrl: "",
+    requirements: [
+      {
+        actionId: "first_guess",
+        description: "Win the game on your first guess",
+        count: 10,
+      },
+    ],
+  },
+  {
+    id: "Animal Lover",
+    description: "Guess 3 animal words",
+    tokens: [
+      {
+        address: process.env.XP_TOKEN_ID,
+        amount: 300,
+      },
+      {
+        address: process.env.REWARD_TOKEN_ID,
+        amount: 200,
+      },
+    ],
+    badgeUrl:
+      "https://duqswponteoedwsylzwn.supabase.co/storage/v1/object/public/badges/Animal%20Lover.png",
+    requirements: [
+      {
+        actionId: "guess_an_animal",
+        description: "Type and enter an animal during a play",
+        count: 3,
+      },
+    ],
+  },
+  {
+    id: "Do You Own a Zoo?",
+    description: "Guess 10 animal words",
+    tokens: [
+      {
+        address: process.env.XP_TOKEN_ID,
+        amount: 1500,
+      },
+      {
+        address: process.env.REWARD_TOKEN_ID,
+        amount: 1000,
+      },
+    ],
+    badgeUrl:
+      "https://duqswponteoedwsylzwn.supabase.co/storage/v1/object/public/badges/Do%20you%20own%20a%20zoo.png",
+    requirements: [
+      {
+        actionId: "guess_an_animal",
+        description: "Type and enter an animal during a play",
+        count: 10,
       },
     ],
   },
