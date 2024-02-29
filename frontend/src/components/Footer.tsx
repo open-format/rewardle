a00ef927e6129203c6c6f28e98afce1632c798b7
Andy <andy@openformat.tech>
2024-02-29 12:21:31 +0000

    add cheeky y margin to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="my-2">
       Made with ❤️ by{" "}
       <Link href="https://www.openformat.tech"> OPENFORMAT</Link>
     </footer>
