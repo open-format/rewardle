ac75013e3dc2b004d6df4ca2d7e763de4063bd62
Andy <andy@openformat.tech>
2024-03-07 11:39:24 +0000

    link to github repo in footer copy

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="my-2">
-      Made with ❤️ by{" "}
-      <Link href="https://www.openformat.tech"> OPENFORMAT</Link>
+      A developer{" "}
+      <Link href="https://github.com/open-format/rewardle">demo </Link> made
+      with ❤️ by <Link href="https://www.openformat.tech"> OPENFORMAT</Link>
     </footer>
   );
 }
