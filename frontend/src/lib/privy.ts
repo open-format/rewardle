bb881d70739b56c210cabdbd787d559b12148d7f
Andy <andy@openformat.tech>
2024-08-02 17:54:10 +0100

    updates loginMessage for privy lib

--- frontend/src/lib/privy.ts
+++ frontend/src/lib/privy.ts
@@ -10,7 +10,7 @@ export const privyConfig: PrivyClientConfig = {
     theme: "dark",
     landingHeader: "Welcome to Rewardle!",
     loginMessage:
-      "A blockchain based word game where you earn tokens by guessing words.",
+      "A blockchain based word game where you earn points, badges and credits for guessing words.",
   },
   defaultChain: arbitrumSepolia,
   loginMethods: ["wallet", "email"],
